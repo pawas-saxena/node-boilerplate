ad1432396130430959c61eeff219abd5aa0e83e1
pawas-saxena <pawas.saxena@gmail.com>
2019-06-20 17:59:17 +0530

    added another route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = config.get('port');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
@@ -20,6 +20,6 @@ app.get('/test', (req, res) => {
     res.render('index.ejs');
 });
 
-app.listen(port, () => {
+app.listen(port, (err) => {
     console.log('server listening on port' + port);
 });
